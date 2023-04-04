31292c9e87bd6fac57d02508c1868b8e332888e4
Sebastian Bøe <sebastian.boe@nordicsemi.no>
2023-04-04 14:28:37 +0200

    Enable tracing
    Signed-off-by: Sebastian Bøe <sebastian.boe@nordicsemi.no>

--- samples/tfm_integration/tfm_secure_partition/src/main.c
+++ samples/tfm_integration/tfm_secure_partition/src/main.c
@@ -14,15 +14,40 @@
 
 #define TIMER_PIN 10
 #define DEBUG_PIN 11
+
 static void my_timer_handler(struct k_timer *dummy)
 {
 }
 
 static K_TIMER_DEFINE(my_timer, my_timer_handler, NULL);
 
+static void conf(uint32_t pin, uint32_t evt)
+{
+	void *handle;
+
+	handle = ppi_trace_config(pin, evt);
+	__ASSERT(handle != NULL,
+		"Failed to initialize trace pin, no PPI or GPIOTE resources?");
+
+	ppi_trace_enable(handle);
+}
+
+void trace_enable(void) {
+	conf(TIMER_PIN,
+		 nrf_rtc_event_address_get(NRF_RTC1_NS, NRF_RTC_EVENT_COMPARE_0));
+
+	NRF_P0->DIRSET |= 1 << DEBUG_PIN;
+
+	NRF_P0->OUTSET |= 1 << DEBUG_PIN;
+	for(volatile int i = 0; i < 1000; i++);
+	NRF_P0->OUTCLR |= 1 << DEBUG_PIN;
+}
+
 void main(void)
 {
 	uint8_t digest[32];
+
+	trace_enable();
 
     k_timer_start(&my_timer, K_USEC(100), K_USEC(300));
 
