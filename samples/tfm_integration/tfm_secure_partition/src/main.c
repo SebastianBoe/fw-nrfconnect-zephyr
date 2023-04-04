559677e99a4fddd14036e0a758f28388204b3df4
Sebastian Bøe <sebastian.boe@nordicsemi.no>
2023-04-04 14:08:15 +0200

    enable timer
    Signed-off-by: Sebastian Bøe <sebastian.boe@nordicsemi.no>

--- samples/tfm_integration/tfm_secure_partition/src/main.c
+++ samples/tfm_integration/tfm_secure_partition/src/main.c
@@ -8,11 +8,23 @@
 #include <tfm_veneers.h>
 #include <tfm_ns_interface.h>
 
+#include <hal/nrf_rtc.h>
+
 #include "dummy_partition.h"
+
+#define TIMER_PIN 10
+#define DEBUG_PIN 11
+static void my_timer_handler(struct k_timer *dummy)
+{
+}
+
+static K_TIMER_DEFINE(my_timer, my_timer_handler, NULL);
 
 void main(void)
 {
 	uint8_t digest[32];
+
+    k_timer_start(&my_timer, K_USEC(100), K_USEC(300));
 
 	for (int key = 0; key < 6; key++) {
 		psa_status_t status = dp_secret_digest(key, digest, sizeof(digest));
